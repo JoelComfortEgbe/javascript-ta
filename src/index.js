ca95c4b5f5c2578e40741daf6593a54c5d58aa8e
JoelComfortEgbe <46760565+JoelComfortEgbe@users.noreply.github.com>
2021-02-04 03:10:08 +0100

    Add existing file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 // complete the function
-function palindrom(str) {
+function palindrome(str) {
   // code goes here
+  let changeStr = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+  let count = 0;
+  if (changeStr===""){
+    console.log("Nothing found!, Please enter  a word");
+    return false;
+  }
+
+  if((changeStr.length) % 2 === 0){
+    count = (changeStr.length) / 2;
+  } else {
+     if (changeStr.length === 1){
+       console.log("Entry is a palindrome.");
+       return true;
+     } else {
+       count = ( changeStr.length - 1) /2;
+     }
+  }
+
+  for ( let x = 0; x < count; x++){
+    if (changeStr[x] != changeStr.slice(-1-x)[0]) {
+      console.log("Entry is not a palindrome.");
+      return false;
+    }
+  }
+  console.log("Entry is a palindrome.");
+  return true;
 }
 
+const string = prompt('Enter a string: ');
+const value = palindrome(string);
+
+console.log(value);
+
+
+/*palindrome('madam');
+palindrome('nurses run');
+palindrome('fox');
+palindrome('eye');
+palindrome('an');*/
+
 function solution(arg) {
-  return palindrom(arg);
+  return palindrome(arg);
 }
 
 if (typeof require !== "undefined" && require.main === module) {
